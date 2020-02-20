24ed5595d35aa2d278dec2c8ee0ec3c3a0d78a11
dmitrij vlasov <dmitrij.vlasov@gmail.com>
2020-02-20 16:06:17 +0200

    Added initial file

---
+++ helper_functions.py
@@ -0,0 +1,73 @@
+import random
+import pygame
+
+
+def is_point_in_snake(square: pygame.Rect, point_x, point_y):
+    """
+    Checks for if the snake hits the apple
+    :param square: Snake in a form of rect
+    :param point_x: apple point in range of snake by x
+    :param point_y: apple point in range of snake by y
+    :return: true if apple point inside snake, else false
+    """
+    if square.x <= point_x <= square.x + square.width and square.y <= point_y <= square.y + square.height:
+        return True
+    else:
+        return False
+
+
+def get_random_color(start_intensity=0, end_intensity=255):
+    """
+    Gets random color for apple
+    :return: Random color for each - red, green, blue
+    """
+    r = random.randint(start_intensity, end_intensity)
+    g = random.randint(start_intensity, end_intensity)
+    b = random.randint(start_intensity, end_intensity)
+    rn_color = r, g, b
+    return rn_color
+
+
+def get_random_position(min_x, max_x, min_y, max_y):
+    """
+    Gets random position for apple
+    :return: Random x and y in screen range
+    """
+    rn_x = random.randint(min_x, max_x)
+    rn_y = random.randint(min_y, max_y)
+    rn_position = rn_x, rn_y
+    return rn_position
+
+
+def get_normal_position(x_pos, y_pos):
+    """
+    Gets a position for snake
+    :param x_pos: x position of snake
+    :param y_pos: y position of snake
+    :return: position x and y in screen range
+    """
+    normal_position = x_pos, y_pos
+    return normal_position
+
+
+def is_point_in_snake(square: pygame.Rect, point_x, point_y):
+    """
+    Checks for if the snake hits the apple
+    :param square: Snake in a form of rect
+    :param point_x: apple point in range of snake by x
+    :param point_y: apple point in range of snake by y
+    :return: true if apple point inside snake, else false
+    """
+    if square.x <= point_x <= square.x + square.width and square.y <= point_y <= square.y + square.height:
+        return True
+    else:
+        return False
+#
+#
+# def get_apple_detected(apple, min_x, max_x, min_y, max_y, snake):
+#     apple_detected = is_point_in_snake(snake, apple.x, apple.y)
+#     if apple_detected:
+#         apple_color = get_random_color()
+#         new_position = get_random_position(min_x, max_x, min_y, max_y)
+#         apple.x = new_position[0]  # set position coordinates
+#         apple.y = new_position[1]  # set position coordinates
--- main.py
+++ main.py
@@ -1,75 +1,38 @@
 import pygame
 import sys
 import random
+import helper_functions
 
 pygame.init()
 
-ZEROINTENSITY = 0  # color range from
-MAXINTENSITY = 255  # color range to
 SNAKE_SIZE = 20
 APPLE_SIZE = 15
 GREEN = 0, 255, 0  # Color of apple
 BLUE = 52, 137, 255  # Color of display
-RED = 255, 0, 0  # Color of box (square)
+RED = 255, 0, 0  # Color of snake (square)
 FPS = 30
-SPEED_X = 3
-SPEED_Y = 3
+SPEED_X = 1
+SPEED_Y = 1
 
 size = SCREEN_WIDTH, SCREEN_HEIGHT = 320, 240  # size of display
-box = pygame.Rect((SCREEN_WIDTH//2) - (SNAKE_SIZE//2), (SCREEN_HEIGHT//2) - (SNAKE_SIZE//2), SNAKE_SIZE, SNAKE_SIZE)  # give coordinates to square
-apple = pygame.Rect(random.randint(0, (SCREEN_WIDTH - APPLE_SIZE)), random.randint(0, (SCREEN_HEIGHT - APPLE_SIZE)), APPLE_SIZE, APPLE_SIZE)
+# give coordinates to snake
+snake = pygame.Rect((SCREEN_WIDTH//2) - (SNAKE_SIZE//2), (SCREEN_HEIGHT//2) - (SNAKE_SIZE//2), SNAKE_SIZE, SNAKE_SIZE)
+# give coordinates to apple
+apple = pygame.Rect(random.randint(0, (SCREEN_WIDTH - APPLE_SIZE)), random.randint(0, (SCREEN_HEIGHT - APPLE_SIZE)),
+                    APPLE_SIZE, APPLE_SIZE)
 move = False  # move by default set to False
 direction = 0, 0  # direction by default set to 0
 apple_color = GREEN
 
-
-def is_point_in_box(square: pygame.Rect, point_x, point_y):
-    """
-    Checks for if the snake hits the apple
-    :param square: Snake in a form of rect
-    :param point_x:
-    :param point_y:
-    :return: if the box point_x and point_y is
-    """
-    if square.x <= point_x <= square.x + square.width and square.y <= point_y <= square.y + square.height:
-        return True
-    else:
-        return False
-
-
-def get_random_color():
-    """
-    Gets random color for apple, each time, when snake hits the apple
-    :return: Random color for each - red, green, blue
-    """
-    r = random.randint(ZEROINTENSITY, MAXINTENSITY)
-    g = random.randint(ZEROINTENSITY, MAXINTENSITY)
-    b = random.randint(ZEROINTENSITY, MAXINTENSITY)
-    rn_color = r, g, b
-    return rn_color
-
-
-def get_random_position():
-    """
-    Gets random position for apple, each time, when snake hits the apple
-    :return: Random x and y in screen range
-    """
-    rn_x = random.randint(0, SCREEN_WIDTH - APPLE_SIZE)
-    rn_y = random.randint(0, SCREEN_HEIGHT - APPLE_SIZE)
-    rn_position = rn_x, rn_y
-    return rn_position
-
-
 screen = pygame.display.set_mode(size)  # create window
 
 while True:  # loop
-    screen.fill(BLUE)  # background blue
-    pygame.draw.rect(screen, RED, box)  # create a square
+    screen.fill(BLUE)  # background
+    pygame.draw.rect(screen, RED, snake)  # create a square
     pygame.draw.rect(screen, apple_color, apple)  # apple
     pygame.display.flip()  # update screen
 
     for event in pygame.event.get():  # get the list of events
-        print(event)
         if event.type == pygame.QUIT:  # if in the list there is event QUIT
             print("Breaking from loop")
             sys.exit()  # then close the window
@@ -86,47 +49,56 @@ while True:  # loop
             direction = (0, -SPEED_Y)
         if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
             move = True  # move is True
-            direction = (0, SPEED_Y)  # direction is down
+            direction = (0, SPEED_Y)
 
     if move is True:
-        box.move_ip(direction[0], direction[1])   # move_ip(0, 0) make box move
+        snake.move_ip(direction[0], direction[1])   # move_ip(0, 0) make snake move
 
-    apple_detected = is_point_in_box(box, apple.x, apple.y)  # function is equals to variable
+    # ---------------------------------------------
+    # function is equals to variable
+    apple_detected = helper_functions.is_point_in_snake(snake, apple.x, apple.y)
     if apple_detected:
-        apple_color = get_random_color()  # give to apple random color after hit
-        new_position = get_random_position()  # give to apple random position after hit
+        # give to apple random color after hit
+        apple_color = helper_functions.get_random_color()
+        # give to apple random position after hit
+        new_position = helper_functions.get_random_position(0, SCREEN_WIDTH - APPLE_SIZE, 0, SCREEN_HEIGHT - APPLE_SIZE)
         apple.x = new_position[0]  # set position coordinates
         apple.y = new_position[1]  # set position coordinates
-    apple_detected = is_point_in_box(box, apple.x + apple.width, apple.y)
+    # ---------------------------------------------
+    apple_detected = helper_functions.is_point_in_snake(snake, apple.x + apple.width, apple.y)
     if apple_detected:
-        apple_color = get_random_color()
-        new_position = get_random_position()
+        apple_color = helper_functions.get_random_color()
+        new_position = helper_functions.get_random_position(0, SCREEN_WIDTH - APPLE_SIZE, 0, SCREEN_HEIGHT - APPLE_SIZE)
         apple.x = new_position[0]
         apple.y = new_position[1]
-    apple_detected = is_point_in_box(box, apple.x, apple.y + apple.height)
+    apple_detected = helper_functions.is_point_in_snake(snake, apple.x, apple.y + apple.height)
     if apple_detected:
-        apple_color = get_random_color()
-        new_position = get_random_position()
+        apple_color = helper_functions.get_random_color()
+        new_position = helper_functions.get_random_position(0, SCREEN_WIDTH - APPLE_SIZE, 0, SCREEN_HEIGHT - APPLE_SIZE)
         apple.x = new_position[0]
         apple.y = new_position[1]
-    apple_detected = is_point_in_box(box, apple.x + apple.width, apple.y + apple.height)
+    apple_detected = helper_functions.is_point_in_snake(snake, apple.x + apple.width, apple.y + apple.height)
     if apple_detected:
-        apple_color = get_random_color()
-        new_position = get_random_position()
+        apple_color = helper_functions.get_random_color()
+        new_position = helper_functions.get_random_position(0, SCREEN_WIDTH - APPLE_SIZE, 0, SCREEN_HEIGHT - APPLE_SIZE)
         apple.x = new_position[0]
         apple.y = new_position[1]
 
-    if box.x + box.width >= SCREEN_WIDTH:  # if box hits the wall, then
-        print("Game Over, you hit the wall")
-        sys.exit()  # close the game
-    if box.x <= 0:
-        print("Game Over, you hit the wall")
-        sys.exit()
-    if box.y + box.height >= SCREEN_HEIGHT:
-        print("Game Over, you hit the wall")
-        sys.exit()
-    if box.y <= 0:
-        print("Game Over, you hit the wall")
-        sys.exit()
+    if snake.x >= SCREEN_WIDTH:  # if snake hits the wall, then
+        snake_new_position = helper_functions.get_normal_position(1, snake.y)
+        snake.x = snake_new_position[0]
+        snake.y = snake_new_position[1]
+    if snake.x + snake.width <= 0:
+        snake_new_position = helper_functions.get_normal_position(SCREEN_WIDTH - SNAKE_SIZE, snake.y)
+        snake.x = snake_new_position[0]
+        snake.y = snake_new_position[1]
+    if snake.y >= SCREEN_HEIGHT:
+        snake_new_position = helper_functions.get_normal_position(snake.x, 1)
+        snake.x = snake_new_position[0]
+        snake.y = snake_new_position[1]
+    if snake.y + snake.height <= 0:
+        snake_new_position = helper_functions.get_normal_position(snake.x, SCREEN_HEIGHT - SNAKE_SIZE)
+        snake.x = snake_new_position[0]
+        snake.y = snake_new_position[1]
 
     pygame.time.Clock().tick(FPS)  # refresh page per sec
